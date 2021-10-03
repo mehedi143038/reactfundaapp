e99fe459a812b5f1e28ff0a4ba6be5ada230d333
mehedi <millathasan125@gmail.com>
2021-10-03 16:19:50 +0600

    new app added

---
+++ src/Components/AddStudent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddStudent = () => {
+    return (
+        <div className='text-center'>
+            <h1> Who are you to add a student? How dare you?</h1>
+        </div>
+    );
+};
+
+export default AddStudent;
---
+++ src/Components/Student.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Student = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Students Data
+                                <Link to={'add-student'} className="btn btn-primary btn-sm float-end">Add Student</Link>
+                            </h4>
+                        </div>
+                        <div className="card-body">
+                            <h3>hello from card body</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Student;
